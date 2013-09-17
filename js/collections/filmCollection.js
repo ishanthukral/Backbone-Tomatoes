1065c98c0e0b13d4ac954daabe4fc9c216e15d9d
Ishan Thukral <dev@ishan.me>
2013-09-17 12:32:32 -0400

    ui fix

--- js/collections/filmCollection.js
+++ js/collections/filmCollection.js
@@ -12,7 +12,7 @@ var FilmCollection = Backbone.Collection.extend({
 		$.ajax({
 			url: 'http://api.rottentomatoes.com/api/public/'+
 			'v1.0/lists/movies/in_theaters.json?'+
-				'page_limit=10&page=1&country=us&'+
+				'page_limit=50&page=1&country=us&'+
 					'apikey=3pug8qv5purwfsbkqfpgb4dc',
 			dataType: "jsonp",
 			success: function(data) {
